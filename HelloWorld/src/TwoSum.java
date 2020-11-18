72d321549a07a3eac30d5f5674edf82002da6519
tsmigaa <tsmigaa@gmail.com>
2020-11-18 08:22:26 +0800

    Two Sum

--- HelloWorld/src/TwoSum.java
+++ HelloWorld/src/TwoSum.java
@@ -1,5 +1,5 @@
 public class TwoSum {
-    
+/*https://leetcode.com/problems/two-sum/*/    
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
